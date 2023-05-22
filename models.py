8caef56a7f2c11eda1d31ca3c84825f71e37a3e7
Nais-Alkator <nais000k@gmail.com>
2023-05-22 22:15:20 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,10 +4,16 @@ from app import app
 
 db = SQLAlchemy(app)
 
+book_author = db.Table('book_author',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
+    db.Column('author_id', db.Integer, db.ForeignKey('author.id'), primary_key=True)
+)
+
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    authors = db.relationship('Author', secondary=book_author, backref=db.backref('books', lazy='dynamic'))
 
 
 class Author(db.Model):
