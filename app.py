e657d1e7e8c24e8c377d7121e518233ee43eaa5f
Nais-Alkator <nais000k@gmail.com>
2023-05-23 16:50:34 +0300

    Add environments

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from routes import blueprint
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 app = Flask(__name__, template_folder="templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.register_blueprint(blueprint)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 if __name__ == '__main__':
