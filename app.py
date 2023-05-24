d0141d2b4bbd166a8c57d6698e37ac92c31fd48e
Nais-Alkator <nais000k@gmail.com>
2023-05-24 10:25:10 +0300

    Add flask debug toolbar

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from routes import blueprint
 from models import db
+from flask_debugtoolbar import DebugToolbarExtension
 
 load_dotenv()
 
@@ -10,9 +11,11 @@ load_dotenv()
 app = Flask(__name__, template_folder="templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(app.root_path, os.environ.get("SQLALCHEMY_DATABASE_URI"))
 app.secret_key = os.environ.get("SECRET_KEY")
-
 db.init_app(app)
+app.debug = True
+toolbar = DebugToolbarExtension(app)
 app.register_blueprint(blueprint)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
