cc9ec6f81aa8109efb60e4fc8ee475eb7b89d76c
Nais-Alkator <nais000k@gmail.com>
2023-05-29 19:55:46 +0300

    Update templates

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ load_dotenv()
 app = Flask(__name__, template_folder="templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(app.root_path, os.environ.get("SQLALCHEMY_DATABASE_URI"))
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-app.config['FLASH_MESSAGES_OPTIONS'] = {'duration': 5000}
 app.secret_key = os.environ.get("SECRET_KEY")
 db.init_app(app)
 app.debug = True
