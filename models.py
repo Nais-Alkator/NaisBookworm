77f3daf0b84df258be7299abe2679a79e9d6b7ca
Nais-Alkator <nais000k@gmail.com>
2023-06-14 00:50:21 +0300

    Update models tests

--- models.py
+++ models.py
@@ -12,17 +12,20 @@ book_author = db.Table('book_author',
 
 
 class Book(db.Model):
+    __tablename__ = 'book'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     authors = db.relationship('Author', secondary=book_author, backref=db.backref('books', lazy='dynamic'))
 
 
 class Author(db.Model):
+    __tablename__ = 'author'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,21 +2,17 @@ import pytest
 from models import Author, Book, User
 
 
+
+
 @pytest.fixture
 def sample_author(app, db):
-    with app.app_context():
-        author = Author(name="Антон Павлович Чехов")
-        db.session.add(author)
-        db.session.commit()
-        return author
+    author = Author(name="Фёдор Михайлович Достоевский")
+    return author
 
 
 def test_author_create(sample_author, app, db):
-    with app.app_context():
-        db.session.add(sample_author)
-        db.session.commit()
-        assert sample_author.name == "Антон Павлович Чехов"
-
+    assert sample_author.name == "Фёдор Михайлович Достоевский"
+        
 
 def test_author_update(sample_author):
     sample_author.name = "Антон Михайлович Чехов"
@@ -24,58 +20,42 @@ def test_author_update(sample_author):
 
 
 @pytest.fixture
-def sample_book(app, sample_author, db):
-    with app.app_context():
-        book = Book(title="Идиот")
-        book.authors = [sample_author]
-        db.session.add(book)
-        db.session.commit()
-        return book
+def sample_book(sample_author):
+    book = Book(title="Идиот")
+    book.authors = [sample_author]
+    return book
+        
 
 
-def test_book_create(sample_book, app, db):
-    with app.app_context():
-        db.session.add(sample_book)
-        db.session.commit()
+def test_book_create(sample_book):
         assert sample_book.title == "Идиот"
-        assert sample_book.authors[0].name == "Антон Павлович Чехов"
+        assert sample_book.authors[0].name == "Фёдор Михайлович Достоевский"
+        
 
-
-def test_book_update(sample_book, app, db):
-    with app.app_context():
-        db.session.add(sample_book)
-        db.session.commit()
-        sample_book.title = "Дама с собачкой"
-        sample_book.authors[0].name = "Антон Михайлович Чехов"
-        assert sample_book.title == "Дама с собачкой"
-        assert sample_book.authors[0].name == "Антон Михайлович Чехов"
-
+def test_book_update(sample_book):
+    sample_book.title = "Дама с собачкой"
+    sample_book.authors[0].name = "Антон Михайлович Чехов"
+    assert sample_book.title == "Дама с собачкой"
+    assert sample_book.authors[0].name == "Антон Михайлович Чехов"
+        
 
 @pytest.fixture
-def sample_user(app, db):
-    with app.app_context():
-        user = User(username="nais", password_hash="12345678", is_active=False)
-        db.session.add(user)
-        db.session.commit()
+def sample_user():
+    user = User(username="asdfgh12345", password_hash="1234567891", is_active=False)
     return user
 
 
 def test_user_create(sample_user, db, app):
-    with app.app_context():
-        db.session.add(sample_user)
-        db.session.commit()
-        assert sample_user.username == "nais"
-        assert sample_user.password_hash == "12345678"
-        assert sample_user.is_active == False
+    assert sample_user.username == "asdfgh12345"
+    assert sample_user.password_hash == "1234567891"
+    assert sample_user.is_active == False
 
 
-def test_user_update(sample_user, app, db):
-    with app.app_context():
-        db.session.add(sample_user)
-        db.session.commit()
-        sample_user.username = "alkator"
-        sample_user.password_hash = "87654321"
-        sample_user.is_active = True
-        assert sample_user.username == "alkator"
-        assert sample_user.password_hash == "87654321"
-        assert sample_user.is_active == True
+def test_user_update(sample_user):
+    sample_user.username = "alkator1"
+    sample_user.password_hash = "87654321"
+    sample_user.is_active = True
+    assert sample_user.username == "alkator1"
+    assert sample_user.password_hash == "87654321"
+    assert sample_user.is_active == True
+        
