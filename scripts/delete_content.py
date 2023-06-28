e2e0d796d90fd408b5af29de71164318f65849ec
Nais-Alkator <nais000k@gmail.com>
2023-06-28 05:11:55 +0300

    Scripts move to conforming directory

---
+++ scripts/delete_content.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path # if you haven't already done so
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError: # Already removed
+    pass
+
+
+from app import app
+from models import db
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.drop_all()
